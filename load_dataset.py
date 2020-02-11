c7bde49c3cc0250fe18d343b65209e1f9dc66b54
kschetan25 <mycoding92@gmail.com>
2020-02-11 02:14:20 -0600

    added loading dataset code

--- load_dataset.py
+++ load_dataset.py
@@ -5,7 +5,8 @@ import cv2
 import urllib
 import csv
 import time
-# import pandas as pd
+import random
+import pickle
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -14,7 +15,6 @@ from tensorflow import keras
 from pylab import rcParams
 from tqdm import tqdm
 from matplotlib import rc
-# from pandas.plotting import register_matplotlib_converters
 from sklearn.model_selection import train_test_split
 from PIL import Image
 
@@ -28,10 +28,10 @@ label_names = ["Dog", "Cat", "Car"]
 # read the image and display
 for label in label_names:
     images_path = os.path.join(dataset_path, label)
-    for image in os.listdir(images_path):
-        sample_image = cv2.imread(os.path.join(
-            images_path, image), cv2.IMREAD_GRAYSCALE)
-        plt.imshow(sample_image, cmap='gray')
+    for clas in os.listdir(images_path):
+        image = cv2.imread(os.path.join(
+            images_path, clas), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(image, cmap='gray')
         plt.show()
         break
     break
@@ -44,3 +44,55 @@ plt.imshow(resized_image, cmap='gray')
 plt.show()
 
 print(resized_image.shape)
+
+# create the training data to feed to train the model
+training_data = []
+
+
+def create_training_data():
+    for label in label_names:
+        images_path = os.path.join(dataset_path, label)
+        label_index = label_names.index(label)
+
+        for clas in tqdm(os.listdir(images_path)):
+            try:
+                cv_image = cv2.imread(os.path.join(
+                    images_path, clas), cv2.IMREAD_GRAYSCALE)
+                resized_image = cv2.resize(cv_image, (image_size, image_size))
+                training_data.append([resized_image, label_index])
+            except Exception as e:
+                pass
+
+
+create_training_data()
+
+# total size of the training dataset(equal to sum of all the images).
+print(len(training_data))
+
+# randomize the training data for better training.
+random.shuffle(training_data)
+
+x_train = []
+y_train = []
+
+for features, label in training_data:
+    x_train.append(features)
+    y_train.append(label)
+
+
+x_train = np.array(x_train).reshape(-1, image_size, image_size, 1)
+
+
+write_x_train = open("x_train.pickle", "wb")
+pickle.dump(x_train, write_x_train)
+write_x_train.close()
+
+write_y_train = open("y_train.pickle", "wb")
+pickle.dump(y_train, write_y_train)
+write_y_train.close()
+
+read_x_train = open("x_train.pickle","rb")
+x_train = pickle.load(read_x_train)
+
+read_y_train = open("y_train.pickle","rb")
+y_train = pickle.load(read_y_train)
